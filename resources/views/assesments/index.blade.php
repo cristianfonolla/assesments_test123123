d8e54a2a32f9f0d1a212fb270249bb88f250c409
cristian <cristian.fonolla@gmail.com>
2016-12-20 16:59:51 +0100

    sync

--- resources/views/assesments/index.blade.php
+++ resources/views/assesments/index.blade.php
@@ -15,9 +15,13 @@
             <div class="col-md-8 col-md-offset-2">
 
 
-                <div class="box box-warning box-solid">
+                <div class="box box-default box-solid">
                     <div class="box-header with-border">
                         <h3 class="box-title">Assesments</h3>
+
+
+
+
 
                         <div class="box-tools pull-right">
                             <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
@@ -28,14 +32,32 @@
                     </div>
                     <!-- /.box-header -->
                     <div class="box-body">
-                        <form role="form">
+
+                        <div class="alert alert-danger alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <h4><i class="icon fa fa-ban"></i> Alert!</h4>
+                            Danger alert preview. This alert is dismissable. A wonderful serenity has taken possession of my entire
+                            soul, like these sweet mornings of spring which I enjoy with my whole heart.
+                        </div>
+
+                        <div class="alert alert-success alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <h4><i class="icon fa fa-check"></i> Alert!</h4>
+                            Success alert preview. This alert is dismissable.
+                        </div>
+
+
+                        <form role="form" action="/assesments" method="post">
+                            {{ csrf_field() }}
+
+                            <input type="hidden" >
                             <!-- text input -->
                             <div class="form-group">
                                 <label>Name</label>
                                 <input type="text" class="form-control" placeholder="Enter ...">
                             </div>
 
-
+                            <input type="submit">
                             {{--<!-- input states -->--}}
                             {{--<div class="form-group has-success">--}}
                             {{--<label class="control-label" for="inputSuccess"><i class="fa fa-check"></i> Input with success</label>--}}
@@ -62,7 +84,7 @@
                     <!-- /.box-body -->
 
                     <div class="box-footer">
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" class="btn btn-danger">Create</button>
                     </div>
                 </div>
 
