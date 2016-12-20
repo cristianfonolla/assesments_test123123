e9d1b0f4c42ef56955c0b31fa7f5dfd4d53f9bbd
cristian <cristian.fonolla@gmail.com>
2016-12-20 16:07:18 +0100

    added box colapsable

--- resources/views/assesments/index.blade.php
+++ resources/views/assesments/index.blade.php
@@ -8,8 +8,41 @@
 @section('main-content')
     <div class="container-fluid spark-screen">
         <div class="row">
+
+
+
+
             <div class="col-md-8 col-md-offset-2">
+
+
+                <div class="box box-warning box-solid">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Assesments</h3>
+
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
+                            </button>
+                        </div>
+                        <!-- /.box-tools -->
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        Aqui van assesments TODO
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+
+
+
+
+
+
+
                 <div class="panel panel-default">
+
+
+
                     <div class="panel-heading">Assesments</div>
 
                     <div class="panel-body">
